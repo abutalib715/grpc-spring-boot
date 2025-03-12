4744baae0e04c8e876e4478adbf387c43fa40eb4
Talib <abutalib715@gmail.com>
2025-03-12 07:05:55 +0600

    fresh spring projetc

---
+++ src/test/java/com/talib/grpc_spring_boot/GrpcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talib.grpc_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
